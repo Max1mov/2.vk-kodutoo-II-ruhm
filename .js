146da4feb4763bcc394cfe8ea5883c74625cb237
Vitali Maksimov <vitamak@greeny.cs.tlu.ee>
2016-06-09 14:28:34 +0300

    proov...

---
+++ .js
@@ -0,0 +1,282 @@
+(function(){
+  "use strict";
+
+  var Raamat = function(){
+    // SINGLETON PATTERN (4 rida)
+    if(Raamat.instance){
+      return Raamat.instance;
+    }
+    Raamat.instance = this; //this viitab raamatule
+
+    this.routes = Raamat.routes;
+
+    //console.log(this);
+
+    //KГµik muutujad, mis on Гјldised ja muudetavad
+    this.currentRoute = null; // hoiab meeles mis lehel hetkel on
+    this.books = []; //kГµik tulevad siia sisse
+
+    //panen rakenduse tГ¶Г¶le
+    this.init();
+  };
+
+  //window.Raamat = Raamat;
+
+  //kirjeldatud kГµik lehed
+  Raamat.routes = {
+    "home-view" : {
+      render: function(){
+        //console.log('JS avalehel');
+      }
+    },
+    "list-view" : {
+      render: function(){
+      //console.log('JS loend lehel');
+      }
+    },
+    "manage-view" : {
+      render: function(){
+        //console.log('JS haldus lehel');
+      }
+    }
+  };
+
+  //kГµik funktsioonid siia sisse
+  Raamat.prototype = {
+    init: function(){
+      //console.log('rakendus kГ¤ivitus');
+      //Esialgne loogika tuleb siia
+      window.addEventListener('hashchange', this.routeChange.bind(this));
+      //vaatan mis lehel olen
+      //console.log(window.location.hash);
+      if(!window.location.hash){
+        window.location.hash = "home-view";
+      }else{
+        //hash oli olemas
+        this.routeChange();
+      }
+      //saan kГ¤tte purgid localStorage kui on
+      if(localStorage.books){
+        //string tagasi objektiks
+        this.books = JSON.parse(localStorage.books);
+        //tekitan loendi htmli
+        this.books.forEach(function(book){
+            var new_book = new Book(book.id, book.BookAuthor, book.BookName, book.timeAdded);
+            var li = new_book.createHtmlElement();
+            document.querySelector('.list-of-books').appendChild(li);
+        });
+      }
+
+      //hakka kuulama hiireklГµpse
+      this.bindEvents();
+    },
+    bindEvents: function(){
+      document.querySelector('.add-new-book').addEventListener('click', this.addNewClick.bind(this));
+      //kuulan trГјkkimist otsi kastist
+      document.querySelector('#search').addEventListener('keyup', this.search.bind(this));
+    },
+    edit: function(event){
+      var selected_id = event.target.dataset.id;
+      var clicked_li = event.target.parentNode;
+      $("#ModalEdit").modal({backdrop: true});
+
+       $(document).on("click", "#edit_close", function(event){
+        return;
+      });
+
+       $(document).on("click", "#save", function(event){
+       console.log(clicked_li);
+       var BookAuthor = document.querySelector('.EditBookAuthor').value;
+       var BookName = document.querySelector('.EditBookName').value;
+       this.books = JSON.parse(localStorage.books);
+       clicked_li.parentNode.removeChild(clicked_li);
+       for(var i=0; i<this.books.length; i++){
+         if(this.books[i].id == selected_id){
+           this.books[i].BookAuthor = BookAuthor;
+           this.books[i].BookName = BookName;
+           break;
+         }
+       }
+       localStorage.setItem('books', JSON.stringify(this.books));
+       location.reload();
+      });
+    },
+    delete: function(event){
+
+      var conf = confirm('Are you sure?');
+      if(!conf){return;}
+      var ul = event.target.parentNode.parentNode;
+      var li = event.target.parentNode;
+      ul.removeChild(li);
+
+      for(var i=0; i<this.books.length; i++){
+        if(this.books[i].id == event.target.dataset.id){
+          //kustuta kohal i objekt Г¤ra
+          this.books.splice(i, 1);
+          //ei lГ¤he edasi
+          break;
+        }
+      }
+      localStorage.setItem('books', JSON.stringify(this.books));
+    },
+    search: function(event){
+      var kokku=0;
+      //otsikasti vГ¤Г¤rtus
+      var needle = document.querySelector('#search').value.toLowerCase();
+      //console.log(needle);
+      var list = document.querySelectorAll('ul.list-of-books li');
+      //console.log(list);
+      for(var i=0; i<list.length; i++){
+        var li = list[i];
+          //Гјhe list itemi sisu
+          var stack = li.querySelector('.content').innerHTML.toLowerCase();
+          //kas otsisГµna on olemas
+          if(stack.indexOf(needle) !== -1){
+            //olemas
+            li.style.display = 'list-item';
+            //this.kokku=list.length;
+          }else{
+            //ei ole olemas
+            li.style.display = 'none';
+          }
+          if(li.style.display == 'list-item'){kokku++;}
+          document.querySelector('#kokku').innerHTML='Kokku: '+kokku;
+      }
+    },
+    addNewClick: function(event){
+      //lisa uus purk
+      var BookAuthor = this.trimWord(document.querySelector('.BookAuthor').value);
+      var BookName = this.trimWord(document.querySelector('.BookName').value);
+	    var timeAdded = this.writeDate();
+
+      //console.log(BookAuthor+' '+BookName+' Lisatud: '+timeAdded);
+	    var className = document.getElementById("show-feedback").className;
+      //lisan masiivi purgid
+
+
+      if(BookAuthor === '' || BookName === ''){
+  		    if(className == "feedback-success"){
+  		        document.querySelector('.feedback-success').className=document.querySelector('.feedback-success').className.replace('feedback-success','feedback-error');
+  		    }
+          document.querySelector('#show-feedback').innerHTML='KГµik read peavad tГ¤idetud olema';
+      }else{
+        if(className == "feedback-error"){
+          document.querySelector('.feedback-error').className=document.querySelector('.feedback-error').className.replace('feedback-error','feedback-success');
+        }
+        document.querySelector('#show-feedback').innerHTML='Salvestamine Гµnnestus';
+  		  var new_book = new Book(guid(), BookAuthor, BookName, timeAdded);
+        //lisan massiivi moosipurgi
+        this.books.push(new_book);
+        //console.log(JSON.stringify(this.books));
+        //JSON'i stringina salvestan local storagisse
+        localStorage.setItem('books', JSON.stringify(this.books));
+        document.querySelector('.list-of-books').appendChild(new_book.createHtmlElement());
+      }
+    },
+    routeChange: function(event){
+      this.currentRoute = window.location.hash.slice(1);
+      //kas leht on olemas
+      if(this.routes[this.currentRoute]){
+        //jah olemas
+        this.updateMenu();
+        //console.log('>>> '+this.currentRoute);
+        //kГ¤ivitan selle lehe jaoks ettenГ¤htud js
+        this.routes[this.currentRoute].render();
+      }else{
+        //404? ei ole
+        //console.log('404');
+        window.location.hash = 'home-view';
+      }
+    },
+    updateMenu: function(){
+      //kui menГјГјl on active-menu siis vГµtame Г¤ra
+      document.querySelector('.active-menu').className=document.querySelector('.active-menu').className.replace(' active-menu', '');
+      //kГ¤esolevale lehele lisan juurde
+      document.querySelector('.'+this.currentRoute).className+=' active-menu';
+    },
+	writeDate : function(){
+		  var d = new Date();
+		  var day = d.getDate();
+		  var month = d.getMonth();
+		  var year = d.getFullYear();
+		  //#clock element htmli
+		  var curTime = this.addZeroBefore(day)+"."+this.addZeroBefore(month+1)+"."+year;
+		  return curTime;
+	},
+	addZeroBefore : function(number){
+		  if(number<10){
+			number="0"+number;
+		  }
+		  return number;
+	},
+    trimWord: function (str) {
+    str = str.replace(/^\s+/, '');
+    for (var i = str.length - 1; i >= 0; i--) {
+        if (/\S/.test(str.charAt(i))) {
+            str = str.substring(0, i + 1);
+            break;
+        }
+    }
+    return str;
+}
+  };
+
+  var Book = function(new_id, BookAuthor, new_BookName, timeAdded){
+    this.id = new_id;
+    this.BookAuthor = BookAuthor;
+    this.BookName = new_BookName;
+	  this.timeAdded = timeAdded;
+  };
+  Book.prototype = {
+    createHtmlElement: function(){
+
+      //anna tagasi ilus html
+      var li = document.createElement('li');
+      var span = document.createElement('span');
+      span.className = 'letter';
+      var letter = document.createTextNode(this.BookAuthor.charAt(0));
+      span.appendChild(letter);
+      li.appendChild(span);
+
+      var content_span = document.createElement('span');
+      content_span.className = 'content';
+      var content = document.createTextNode(this.BookAuthor+' | '+this.BookName+' Lisatud: '+this.timeAdded+' ');
+      content_span.appendChild(content);
+      li.appendChild(content_span);
+
+      var delete_span = document.createElement('button');
+      delete_span.setAttribute('data-id', this.id);
+      delete_span.innerHTML = "Kustuta";
+      li.appendChild(delete_span);
+      delete_span.addEventListener('click', Raamat.instance.delete.bind(Raamat.instance));
+
+      var edit_span = document.createElement('button');
+      edit_span.setAttribute('data-id', this.id);
+      edit_span.innerHTML = "Muuda";
+      li.appendChild(edit_span);
+      edit_span.addEventListener('click', Raamat.instance.edit.bind(Raamat.instance));
+
+      //console.log(li);
+      return li;
+    }
+  };
+
+  //helper
+  function guid(){
+    var d = new Date().getTime();
+    if(window.performance && typeof window.performance.now === "function"){
+        d += performance.now(); //use high-precision timer if available
+    }
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+  }
+
+  window.onload = function(){
+    var app = new Raamat();
+  };
+
+})();
--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 (function(){
    "use strict";
 
-   var Tegevused = function(){
+   var Tegevus = function(){
 
      // SEE ON SINGLETON PATTERN
-     if(Tegevused.instance){
-       return Tegevused.instance;
+     if(Tegevus.instance){
+       return Tegevus.instance;
      }
      //this viitab Tegevus fn
-     Tegevused.instance = this;
+     Tegevus.instance = this;
 
-     this.routes = Tegevused.routes;
+     this.routes = Tegevus.routes;
      // this.routes['home-view'].render()
 
      console.log('Tegevused sees');
@@ -27,7 +27,7 @@
    //window.Moosipurk = Moosipurk; // Paneme muuutja külge
 
    //kirjeldatud kõik lehed
-   Tegevused.routes = {
+   Tegevus.routes = {
      "home-view" : {
        render: function(){
          console.log('JS avalehel');
@@ -46,7 +46,7 @@
    };
 
    // Kõik funktsioonid lähevad Moosipurgi külge
-   Tegevused.prototype = {
+   Tegevus.prototype = {
 
      init: function(){
        console.log('Rakendus läks tööle');
@@ -66,8 +66,8 @@
        //saan kätte purgid localStorage kui on
        if(localStorage.teges){
            //võtan stringi ja teen tagasi objektideks
-           this.tege = JSON.parse(localStorage.teges);
-           console.log('laadisin localStorageist massiiivi ' + this.jars.length);
+           this.teges = JSON.parse(localStorage.teges);
+           console.log('laadisin localStorageist massiiivi ' + this.teges.length);
 
            //tekitan loendi htmli
            this.teges.forEach(function(jar){
@@ -88,7 +88,7 @@
      },
 
      bindEvents: function(){
-       document.querySelector('.add-new-teges').addEventListener('click', this.addNewClick.bind(this));
+       document.querySelector('.add-new-tege').addEventListener('click', this.addNewClick.bind(this));
 
        //kuulan trükkimist otsikastis
        document.querySelector('#search').addEventListener('keyup', this.search.bind(this));
@@ -105,13 +105,13 @@
 
         $(document).on("click", "#save", function(event){
             console.log(clicked_li);
-          var Tegevus = document.querySelector('.EditTegevus').value;
+          var Nimetus = document.querySelector('.EditNimetus').value;
           var Prioriteet = document.querySelector('.EditPrioriteet').value;
           this.teges = JSON.parse(localStorage.teges);
-          clicked_li.parentNode.removeChild(klops);
+          clicked_li.parentNode.removeChild(clicked_li);
         for(var i=0; i<this.teges.length; i++){
           if(this.teges[i].id == selected_id){
-            this.teges[i].Tegevus = Tegevus;
+            this.teges[i].Nimetus = Nimetus;
             this.teges[i].Prioriteet = Prioriteet;
             break;
           }
@@ -136,4 +136,197 @@
 
 		ul.removeChild(li);
 
-		//KUSTUTAN OBJEKTI ja uuenda localSt
+		//KUSTUTAN OBJEKTI ja uuenda localStoragit
+
+    for(var i=0; i<this.teges.length; i++){
+      if(this.teges[i].id == event.target.dataset.id){
+        //kustuta kohal i objekt ära
+        this.teges.splice(i, 1);
+        //ei lähe edasi
+        break;
+      }
+    }
+    localStorage.setItem('teges', JSON.stringify(this.teges));
+	 },
+     search: function(event){
+         //otsikasti väärtus
+         var needle = document.querySelector('#search').value.toLowerCase();
+         console.log(needle);
+
+         var list = document.querySelectorAll('ul.list-of-teges li');
+         console.log(list);
+
+         for(var i = 0; i < list.length; i++){
+
+             var li = list[i];
+
+             // ühe listitemi sisu tekst
+             var stack = li.querySelector('.content').innerHTML.toLowerCase();
+
+             //kas otsisõna on sisus olemas
+             if(stack.indexOf(needle) !== -1){
+                 //olemas
+                 li.style.display = 'list-item';
+
+             }else{
+                 //ei ole, index on -1, peidan
+                 li.style.display = 'none';
+
+             }
+             if(li.style.display == 'list-item'){kokku++;}
+             document.querySelector('#kokku').innerHTML='Kokku: '+kokku;
+         }
+     },
+
+     addNewClick: function(event){
+       //salvestame purgi
+       //console.log(event);
+
+       var Nimetus = this.trimWord(document.querySelector('.Nimetus').value);
+       var Prioriteet = this.trimWord(document.querySelector('.Prioriteet').value);
+       var timeAdded = this.writeDate();
+
+
+       //  nimetus + prioriteet + timeAdded
+      var className = document.getElementById("show-feedback").className;
+      //lisan masiivi purgid
+
+
+      if(Nimetus === '' || Prioriteet === ''){
+          if(className == "feedback-success"){
+              document.querySelector('.feedback-success').className=document.querySelector('.feedback-success').className.replace('feedback-success','feedback-error');
+          }
+          document.querySelector('#show-feedback').innerHTML='Kõik read peavad täidetud olema';
+      }else{
+        if(className == "feedback-error"){
+          document.querySelector('.feedback-error').className=document.querySelector('.feedback-error').className.replace('feedback-error','feedback-success');
+        }
+        document.querySelector('#show-feedback').innerHTML='Salvestamine õnnestus';
+        var new_tege = new Tege(guid(), Nimetus, Prioriteet, timeAdded);
+        //lisan massiivi moosipurgi
+        this.teges.push(new_tege);
+        //JSON'i stringina salvestan local storagisse
+        localStorage.setItem('teges', JSON.stringify(this.teges));
+        document.querySelector('.list-of-teges').appendChild(new_teges.createHtmlElement());
+
+
+			}
+		},
+
+    routeChange: function(event){
+      this.currentRoute = window.location.hash.slice(1);
+      //kas leht on olemas
+      if(this.routes[this.currentRoute]){
+        //jah olemas
+        this.updateMenu();
+
+        this.routes[this.currentRoute].render();
+      }else{
+        //404? ei ole
+        //console.log('404');
+        window.location.hash = 'home-view';
+      }
+    },
+     updateMenu: function() {
+       //http://stackoverflow.com/questions/195951/change-an-elements-class-with-javascript
+       //1) võtan maha aktiivse menüülingi kui on
+       document.querySelector('.active-menu').className = document.querySelector('.active-menu').className.replace('active-menu', '');
+
+       //2) lisan uuele juurde
+       //console.log(location.hash);
+       document.querySelector('.'+this.currentRoute).className += ' active-menu';
+ 	},
+  writeDate : function(){
+      var d = new Date();
+      var day = d.getDate();
+      var month = d.getMonth();
+      var year = d.getFullYear();
+      //#clock element htmli
+      var curTime = this.addZeroBefore(day)+"."+this.addZeroBefore(month+1)+"."+year;
+      return curTime;
+  },
+  addZeroBefore : function(number){
+      if(number<10){
+      number="0"+number;
+      }
+      return number;
+  },
+    trimWord: function (str) {
+    str = str.replace(/^\s+/, '');
+    for (var i = str.length - 1; i >= 0; i--) {
+        if (/\S/.test(str.charAt(i))) {
+            str = str.substring(0, i + 1);
+            break;
+        }
+    }
+    return str;
+  }
+    };
+  var Tege = function(new_id, Nimetus, new_Prioriteet, timeAdded){
+  this.id = new_id;
+    this.Nimetus = Nimetus;
+    this.Prioriteet = new_Prioriteet;
+    this.timeAdded = timeAdded;
+    console.log('created new tege');
+  };
+
+   Tege.prototype = {
+     createHtmlElement: function(){
+
+       var li = document.createElement('li');
+
+       var span = document.createElement('span');
+       span.className = 'letter';
+
+       var letter = document.createTextNode(this.Nimetus.charAt(0));
+       span.appendChild(letter);
+
+       li.appendChild(span);
+
+       var span_with_content = document.createElement('span');
+       span_with_content.className = 'content';
+
+       var content = document.createTextNode(this.Nimetus + ' | ' + this.Prioriteet + ' | ' + this.timeAdded);
+       span_with_content.appendChild(content);
+
+       li.appendChild(content_span);
+
+	   //DELETE nupp
+     var delete_span = document.createElement('button');
+     delete_span.setAttribute('data-id', this.id);
+     delete_span.innerHTML = "Kustuta";
+     li.appendChild(delete_span);
+     delete_span.addEventListener('click', Tegevus.instance.delete.bind(Tegevus.instance));
+
+     var edit_span = document.createElement('button');
+     edit_span.setAttribute('data-id', this.id);
+     edit_span.innerHTML = "Muuda";
+     li.appendChild(edit_span);
+     edit_span.addEventListener('click', Tegevus.instance.edit.bind(Tegevus.instance));
+
+
+       return li;
+
+     }
+   };
+
+   //HELPER
+   function guid(){
+		var d = new Date().getTime();
+		if(window.performance && typeof window.performance.now === "function"){
+			d += performance.now(); //use high-precision timer if available
+		}
+		var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = (d + Math.random()*16)%16 | 0;
+			d = Math.floor(d/16);
+			return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+		});
+		return uuid;
+	}
+
+   // kui leht laetud käivitan Moosipurgi rakenduse
+   window.onload = function(){
+     var app = new Tegevus();
+   };
+
+})();
